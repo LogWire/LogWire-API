0b6188d5ee1f226a87db2eac55d74427d0e30c7f
Linternator <up607958@myport.ac.uk>
2020-02-27 23:14:57 +0000

    Initial commit with basic status endpoint WIP.

---
+++ LogWire-API/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LogWire.Controller.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogWire.API.Controllers
+{
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/status/system")]
+        public async Task<ActionResult> GetStatus()
+        {
+            var value = await StatusApiClient.GetSystemStatus("https://localhost:5001", "e29e7516-829a-4e07-9e0f-a7ad7b3f27ec");
+
+            return Ok(value);
+        }
+
+    }
+
+}
