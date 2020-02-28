13c82d35f9892cc0752b237d2088cdd1fac8d06b
Linternator <up607958@myport.ac.uk>
2020-02-28 20:35:43 +0000

    Added basic login endpoint without tokens.

---
+++ LogWire-API/Controllers/AuthenticationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LogWire.API.Utils;
+using LogWire.Controller.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace LogWire.API.Controllers
+{
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        [HttpPost]
+        [Route("/auth/login")]
+        public async Task<ActionResult> Login()
+        {
+
+            if (Request.HasHeader("username") && Request.HasHeader("password"))
+            {
+
+                var value = await AuthenticationApiClient.Login(_configuration["controller_endpoint"], _configuration["access_token"], Request.Headers["username"], Request.Headers["password"]);
+
+                if (!String.IsNullOrWhiteSpace(value))
+                {
+                    return Ok(new { UserId = value });
+                }
+
+                return BadRequest(new { Message = "Error with username or password." });
+
+            }
+
+            return BadRequest(new { Message = "Missing headers" });
+
+        }
+
+    }
+}
---
+++ LogWire-API/Utils/HeaderUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace LogWire.API.Utils
+{
+    public static class HeaderUtils
+    {
+
+        public static bool HasHeader(this HttpRequest request, string header)
+        {
+            return request.Headers.ContainsKey(header);
+        }
+
+    }
+}
